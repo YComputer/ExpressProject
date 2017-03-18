a571fea0683dd47f69157c92c4cbef0a6e24f334
yaohongwei2012 <yaohongwei2012@126.com>
2017-03-18 18:40:20 +0800

    add message model

---
+++ model/message.js
@@ -0,0 +1,42 @@
+/**
+ * Created by yaohw on 2017-03-18 14:25:03.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var BaseModel = require("./base_model");
+
+var config = require('../config').config;
+
+//数据库连接
+// mongoose.connect(config.mongo_db, {
+//     server: { poolSize: 20 }
+// }, function (err) {
+//     if (err) {
+//         logger.error('connect to %s error: ', config.mongo_db, err.message);
+//         process.exit(1);
+//     }
+// });
+
+//留言板Schema
+var MessageSchema = new Schema({
+    title: { type: String },//主题
+    content: { type: String },//内容
+    author: { type: ObjectId },//作者
+    email: { type: String },//邮箱
+    tel: { type: String },//电话
+    uploadTime: { type: Date, default: Date.now }//发布时间
+});
+
+//创建索引
+MessageSchema.index({title: 1});
+
+//保存前预处理
+MessageSchema.pre('save', function (next) {
+    var now = new Date();
+    this.uploadTime = now;
+    next();
+});
+
+exports.messageModel = mongoose.model('message', MessageSchema);
