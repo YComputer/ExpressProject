77f53a48a199638d1a32c76afe8ab10be4bc888f
niuxintao <niuxintao1123@hotmail.com>
2017-04-24 08:27:44 +0800

    direct use of the scratch editor

---
+++ controller/create.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hr on 2016/11/29.
+ */
+var logger = require('../common/logger');
+var config     = require('../config');
+var url = require("url");
+
+exports.showEditor = function(req, res, next){
+    res.render('create', { title: 'Create' });
+}
