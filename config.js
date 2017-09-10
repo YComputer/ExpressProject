1d519f4cab91ca297e19c923ceeca28a71c0492f
root <root@iZbp19gg1p5y1cs4u3qgm5Z.(none)>
2017-09-10 15:38:20 +0800

    edit config.js and mail

--- config.js
+++ config.js
@@ -4,23 +4,23 @@
 var config = {
     port: 5000,
     // mongodb 配置
-    mongo_db: 'mongodb://127.0.0.1:27017/test',
+    mongo_db: 'mongodb://121.196.202.96:27017/test',
 
     //redis 配置
     redis_config: {
         redis_port: "6379",
-        redis_host: "127.0.0.1",
+        redis_host: "121.196.202.96",
         redis_db: 0,
         redis_password: ""
     },
     session_secret: '01space_org',
     auth_cookie_name: '01space',
     project_base_path: "/mnt/c/Users/xintao/Desktop/website/ExpressProject/",//"/com/bin/ExpressProject/",
-    host: 'http://127.0.0.1:5000/',
+    host: 'http://121.196.202.96:80/',
     mailSystem: {
-        host: 'smtp.sina.cn',
-        secureConnection: true,
-        port: 25,
+        host: 'smtp.sina.com',
+        secure: true,
+        port: 465,
         auth: {
             user: '01_space@sina.cn',
             pass: 'lijie19871108'
